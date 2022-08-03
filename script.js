3b527200797b6e136620f94759231ad88f98a4bf
Michael Hanley <michael.hanley@mycit.ie>
2022-08-03 15:35:58 +0100

    added functionality to click for larger profile picture

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function aboutHandler(){
-    alert('hit')
+function aboutHandler() {
+  alert("hit");
 }
+
+function pictureHandler(set) {
+  const overlay = document.getElementById("overlay");
+
+  const pictureContainer = document.getElementById("profilePicContainer");
+
+  const outerPictureContainer = document.getElementById(
+    "outerProfilePicContainer"
+  );
+
+  if (set) {
+    overlay.style.display = "unset";
+    pictureContainer.setAttribute("class", "pictureContainerClicked");
+    outerPictureContainer.setAttribute("class", "outerpicContainerClicked");
+  } else {
+    overlay.style.display = "none";
+    pictureContainer.setAttribute("class", "profilePicContainerPostClick");
+    outerPictureContainer.setAttribute("class", "outerProfilePicContainer");
+    setTimeout(function () {
+      pictureContainer.setAttribute("class", "profilePicContainer");
+    }, 300);
+  }
+}
+
+function overlayHandler() {}
