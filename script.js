6695dd0a55c6e940e7eb7b7117c049654088cc7e
Michael Hanely <michael.hanley@mycit.ie>
2022-08-12 16:51:25 +0100

    added my projects section & fixed overlay bug

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function pictureHandler() {
-  const overlay = document.getElementById("overlay");
+  document.getElementById("overlayContainer").style.display = "flex";;
+  document.getElementById('borderForFoccussedProfilePic').style.display = 'flex'
+  document.getElementById('focussedProfilePic').style.display = 'flex'
 
-  overlay.style.display = "flex";
 
 }
 function clickOutHandler() {
-  const overlay = document.getElementById("overlay");
+  document.getElementById("overlayContainer").style.display = "none";;
+  document.getElementById('borderForFoccussedProfilePic').style.display = 'none'
+  document.getElementById('focussedProfilePic').style.display = 'none'
+  document.getElementById('projectsList').style.display = 'none'
 
-  overlay.style.display = "none";
 
+
+}
+function projectsHandler() {
+  document.getElementById("overlayContainer").style.display = "flex";;
+
+  document.getElementById('projectsList').style.display = 'flex'
 }
